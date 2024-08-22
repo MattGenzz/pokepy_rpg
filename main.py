97df00eb1b9b7b53ce21869e15446976e425b593
mattgenzz <matheus_genz@hotmail.com>
2024-08-22 15:35:40 -0300

    [feat] first commit: criação de settings e inicialização da main.

---
+++ main.py
@@ -0,0 +1,30 @@
+from settings import *
+from pytmx.util_pygame import load_pygame
+from os.path import join
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        pygame.display.set_caption('Monster hunter')
+        
+        self.import_assets()
+    
+    def import_assets(self):
+        self.tmx_maps = {'world': load_pygame(join('data', 'maps', 'world.tmx'))}
+        print(self.tmx_maps)
+        
+    def run(self):
+        while True:
+            # event loop
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    exit()
+                    
+            # game logic
+            pygame.display.update()
+            
+if __name__ == '__main__':
+    game = Game()
+    game.run()
